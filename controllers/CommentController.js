8d755b385902f5eac26070ada94319e91b0cf7f0
sosiksaWteste <galnikser@gmail.com>
2025-04-21 06:32:47 +0300

    initial changes

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,39 +1,62 @@
 const {Comment, User} = require('../models/Relations');
 const jwt = require('jsonwebtoken');
+const NodeCache = require('node-cache');
+
+const commentsCache = new NodeCache({ stdTTL: 0, checkperiod: 120 });
+
+function getFromCache(key) {
+    return commentsCache.get(key);
+}
+function setCache(key, data) {
+    return commentsCache.set(key, data);
+}
 
 const getAllComments = async (req, res) => {
     try {
+        const cachedComments = getFromCache('allComments');
+        if (cachedComments) {
+            console.log('Returning comments from cache');
+            return res.status(201).json(cachedComments);
+        }
         const comments = await Comment.findAll({
             attributes: ['comment_id', 'content', 'post_id', 'user_id'],
         });
+        setCache('allComments', comments);
         res.status(200).json(comments);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching comments', error });
     }
-}
+};
 
 const getCommentsByPostId = async (req, res) => {
     try {
         const post_id = req.params.id;
+        const cachedComments = getFromCache(`commentsPostId:${post_id}`);
+        if (cachedComments) {
+            console.log('Returning comments from cache');
+            return res.status(201).json(cachedComments);
+        }
         const comments = await Comment.findAll({
             where: { post_id: post_id },
             include: [
                 {
                     model: User,
                     as: 'user',
-                    attributes: ['login']
-                }
+                    attributes: ['login'],
+                },
             ],
-            attributes: { exclude: ['user_id'] }
+            attributes: { exclude: ['user_id'] },
         });
-        if (!comments) {
+        if (!comments || comments.length === 0) {
             return res.status(404).json({ message: 'Comments not found' });
         }
+        setCache(`commentsPostId:${post_id}`, comments);
         res.status(200).json(comments);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching comments', error });
     }
-}
+};
+
 
 const createComment = async (req, res) => {
     try {
@@ -45,11 +68,13 @@ const createComment = async (req, res) => {
             return res.status(401).json({ message: 'Unauthorized' });
         }
         const comment = await Comment.create({ content, post_id, user_id: user.user_id });
+        commentsCache.del('allComments');
+        commentsCache.del(`commentsPostId:${post_id}`);
         res.status(201).json(comment);
     } catch (error) {
         res.status(500).json({ message: 'Error creating comment', error });
     }
-}
+};
 
 module.exports = {
     getAllComments,
--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,8 +1,22 @@
 const {User, Post, Category} = require('../models/Relations');
 const jwt = require('jsonwebtoken');
+const NodeCache = require('node-cache');
+
+const postsCache = new NodeCache({ stdTTL: 0, checkperiod: 120 });
+
+function getFromCache(key) {
+    return postsCache.get(key);
+}
+function setCache(key, data) {
+    return postsCache.set(key, data);
+}
 
 const getAllPosts = async (req, res) => {
     try {
+        const cachedPosts = getFromCache('allPosts');
+        if (cachedPosts) {
+            return res.status(201).json(cachedPosts);
+        }
         const posts = await Post.findAll({
             include: [
                 {
@@ -16,17 +30,22 @@ const getAllPosts = async (req, res) => {
                     attributes: ['name']
                 }
             ],
-            attributes: {exclude: ['user_id', 'category_id']},
+            attributes: { exclude: ['user_id', 'category_id'] },
         });
+        setCache('allPosts', posts);
         res.status(200).json(posts);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching posts', error });
     }
-}
+};
 
 const getPostById = async (req, res) => {
     try {
         const postId = req.params.id;
+        const cachedPost = getFromCache(`post:${postId}`);
+        if (cachedPost) {
+            return res.status(201).json(cachedPost);
+        }
         const post = await Post.findOne({
             where: { post_id: postId },
             include: [
@@ -46,11 +65,12 @@ const getPostById = async (req, res) => {
         if (!post) {
             return res.status(404).json({ message: 'Post not found' });
         }
+        setCache(`post:${postId}`, post);
         res.status(200).json(post);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching post', error });
     }
-}
+};
 
 const createPost = async (req, res) => {
     try {
@@ -59,21 +79,28 @@ const createPost = async (req, res) => {
         if (!token) {
             return res.status(401).json({ message: 'Unauthorized' });
         }
-        let category = await Category.findOne({ where: { name: category_name } });
-        if (!category) {
-            category = Category.create({ name: category_name });
-        }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findByPk(decoded.id);
         if (!user) {
             return res.status(401).json({ message: 'Unauthorized' });
         }
-        const post = await Post.create({ title, content, user_id: user.user_id, category_id: category.category_id });
+        let category = await Category.findOne({ where: { name: category_name } });
+        if (!category) {
+            category = await Category.create({ name: category_name });
+        }
+        const post = await Post.create({
+            title,
+            content,
+            user_id: user.user_id,
+            category_id: category.category_id
+        });
+        postsCache.del('allPosts')
+        setCache(`post:${post.post_id}`, post);
         res.status(201).json(post);
     } catch (error) {
         res.status(500).json({ message: 'Error creating post', error });
     }
-}
+};
 
 module.exports = {
     getAllPosts,
